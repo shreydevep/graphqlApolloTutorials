e23bc57e24474639ab7aee4d9638910db241e86c
Shreyas Pandey <shrey.devep@gmail.com>
2022-05-18 11:25:27 +0530

    Query

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,9 +1,10 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,12 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete description,can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "List of Module"
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -28,6 +35,15 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in minutes"
+    length: Int
   }
 `;
 
